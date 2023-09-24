8bebdc7a8340ee925a1d8fcb334fd43e298c9fc9
kbereza <kbereza@linux.myguest.virtualbox.org>
2023-09-24 16:40:30 +0300

    new

---
+++ project/build/CMakeFiles/file.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kbereza/File-handler/project/build/file_autogen/mocs_compilation.cpp" "CMakeFiles/file.dir/file_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/file.dir/file_autogen/mocs_compilation.cpp.o.d"
+  "/home/kbereza/File-handler/project/main.cpp" "CMakeFiles/file.dir/main.cpp.o" "gcc" "CMakeFiles/file.dir/main.cpp.o.d"
+  "/home/kbereza/File-handler/project/mainwindow.cpp" "CMakeFiles/file.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/file.dir/mainwindow.cpp.o.d"
+  "/home/kbereza/File-handler/project/worker.cpp" "CMakeFiles/file.dir/worker.cpp.o" "gcc" "CMakeFiles/file.dir/worker.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/build/CMakeFiles/file.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/file_autogen.dir/ParseCache.txt"
+  "file_autogen"
+  "CMakeFiles/file.dir/file_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/file.dir/file_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/file.dir/main.cpp.o"
+  "CMakeFiles/file.dir/main.cpp.o.d"
+  "CMakeFiles/file.dir/mainwindow.cpp.o"
+  "CMakeFiles/file.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/file.dir/worker.cpp.o"
+  "CMakeFiles/file.dir/worker.cpp.o.d"
+  "file"
+  "file.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
